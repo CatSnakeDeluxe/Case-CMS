6de04bd3a20752bcfac21a95a48da002388058a7
Johanna Backlund Wass <johanna.bw1@gmail.com>
2023-03-29 11:36:09 +0200

    up and running on linode

--- app/public/database.php
+++ app/public/database.php
@@ -1,8 +1,15 @@
 <?php
-    define('DB_SERVER', 'mysql');
-    define('DB_USERNAME', 'db_user');
-    define('DB_PASSWORD', 'db_password');
-    define('DB_NAME', 'db_lamp_app');
+    // LINODE
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'cms_user_linode');
+    define('DB_PASSWORD', 'ZY3eDmRchz');
+    define('DB_NAME', 'cms');
+
+    // LOCAL
+    // define('DB_SERVER', 'mysql');
+    // define('DB_USERNAME', 'db_user');
+    // define('DB_PASSWORD', 'db_password');
+    // define('DB_NAME', 'db_lamp_app');
 
     try {
         // connect to db
